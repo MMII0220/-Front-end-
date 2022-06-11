9e034e4572785b8cf7db46384d70261d5db0ac43
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-06-11 18:24:02 +0300

    adding localStorage

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const cats = document.querySelectorAll('.cats'),
-    hearth__two = document.querySelectorAll('.hearth__two'),
+  const hearth__two = document.querySelectorAll('.hearth__two'),
     hearth__three = document.querySelectorAll('.hearth__three'),
     link = document.querySelectorAll('.link'),
     arr = [];
 
-  /* let a, b;
+  let itemsArray = [],
+    item;
 
-  localStorage.setItem('a', hearth__two[0]);
+  let stored = localStorage.getItem('items');
 
-  a = localStorage.getItem('a');
+  if (stored) ;
 
-  console.log(a); */
-
+  /*Clicked Hearth
+   ********************/
   const click__hearth = () => {
-    /*Clicked Hearth
-     ********************/
     hearth__two.forEach((item, i) => {
       item.addEventListener('click', () => {
         if (hearth__three[i].classList.contains != 'hide') {
@@ -23,6 +21,12 @@ window.addEventListener('DOMContentLoaded', () => {
           hearth__three[i].classList.add('show');
 
           arr.push(hearth__three[i].parentNode);
+
+          item = itemsArray.push(hearth__three[i].parentNode);
+
+          localStorage.setItem('items', item);
+
+          stored = localStorage.getItem('items');
         }
       });
     });
@@ -41,30 +45,40 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  click__hearth();
-  unclick__hearth();
-
   /* Add Cats to Favorite
    *************************/
-  link[1].addEventListener('click', () => {
-    hearth__three.forEach((item, i) => {
-      /* if (item.classList.contains != 'hide') {
-        arr.push(cats[i]);
-      } */
-      if (item.classList.contains != 'show') {
-        item.parentNode.classList.remove('show');
-        item.parentNode.classList.add('hide');
-      }
+  const add__favorite = () => {
+    link[1].addEventListener('click', () => {
+      hearth__three.forEach((item) => {
+        if (item.classList.contains != 'flex') {
+          item.parentNode.classList.remove('flex');
+          item.parentNode.classList.add('hide');
+        }
+      });
+
+      arr.forEach((key) => {
+        key.append();
+        key.classList.remove('hide');
+        key.classList.add('flex');
+      });
     });
+  };
 
-    arr.forEach((key) => {
-      key.append();
-      key.classList.remove('hide');
-      key.classList.add('show');
+  /* Return All Cats Back
+   ******************************/
+  const return__cats = () => {
+    link[0].addEventListener('click', () => {
+      hearth__three.forEach((item) => {
+        if (item.classList.contains != 'hide') {
+          item.parentNode.classList.remove('hide');
+          item.parentNode.classList.add('flex');
+        }
+      });
     });
+  };
 
-    console.log(arr);
-  });
-
-  /* console.log((hearth__three[0].parentNode.style.display = 'none')); */
+  click__hearth();
+  unclick__hearth();
+  return__cats();
+  add__favorite();
 });

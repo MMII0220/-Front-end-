34274fca066dd1047a1a802c4b17efc1ea2d69cc
eneegmatic <roziev0001@gmail.com>
2022-06-12 23:01:51 +0500

    like click added

--- main.js
+++ main.js
@@ -1,70 +1,54 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const cats = document.querySelectorAll('.cats'),
-    hearth__two = document.querySelectorAll('.hearth__two'),
-    hearth__three = document.querySelectorAll('.hearth__three'),
-    link = document.querySelectorAll('.link'),
-    arr = [];
+const catContainer = document.querySelector('.cats__collection');
+// const cats = [
+//   {
+//     img: "./assets/img/cat1.png",
+//     id: 1,
+//     likedByMe: false,
+//   },
+//   {
+//     img: "./assets/img/cat2.png",
+//     id: 2,
+//     likedByMe: true,
+//   },
+//   {
+//     img: "./assets/img/cat3.png",
+//     id: 3,
+//     likedByMe: false,
+//   },
+//   {
+//     img: "./assets/img/cat4.png",
+//     id: 4,
+//     likedByMe: false,
+//   }
+// ];
+const cats = JSON.parse(localStorage.getItem('cats')) || [];
 
-  /* let a, b;
-
-  localStorage.setItem('a', hearth__two[0]);
-
-  a = localStorage.getItem('a');
-
-  console.log(a); */
-
-  const click__hearth = () => {
-    /*Clicked Hearth
-     ********************/
-    hearth__two.forEach((item, i) => {
-      item.addEventListener('click', () => {
-        if (hearth__three[i].classList.contains != 'hide') {
-          hearth__three[i].classList.remove('hide');
-          hearth__three[i].classList.add('show');
-
-          arr.push(hearth__three[i].parentNode);
-        }
-      });
-    });
-  };
-
-  /* Remove Click Effect
-   ***********************/
-  const unclick__hearth = () => {
-    hearth__three.forEach((item, i) => {
-      item.addEventListener('click', () => {
-        if (hearth__three[i].classList.contains != 'show') {
-          hearth__three[i].classList.remove('show');
-          hearth__three[i].classList.add('hide');
-        }
-      });
-    });
-  };
-
-  click__hearth();
-  unclick__hearth();
-
-  /* Add Cats to Favorite
-   *************************/
-  link[1].addEventListener('click', () => {
-    hearth__three.forEach((item, i) => {
-      /* if (item.classList.contains != 'hide') {
-        arr.push(cats[i]);
-      } */
-      if (item.classList.contains != 'show') {
-        item.parentNode.classList.remove('show');
-        item.parentNode.classList.add('hide');
-      }
-    });
-
-    arr.forEach((key) => {
-      key.append();
-      key.classList.remove('hide');
-      key.classList.add('show');
-    });
-
-    console.log(arr);
+const setCats = () => {
+  cats.forEach(cat => {
+    const catItem = document.createElement('div');
+    catItem.classList.add('cat');
+    catItem.addEventListener('click',( ) =>handleLiked(cat.id) )
+    catItem.innerHTML = `
+      <div>
+      <img src="${cat.img}" alt="cat">
+      ${cat.likedByMe ? '<div>liked<div>' : ''}
+      </div>
+    `
+    catContainer.appendChild(catItem);
   });
+}
 
-  /* console.log((hearth__three[0].parentNode.style.display = 'none')); */
-});
+const handleLiked = (id) => {
+  cats.forEach(cat => {
+    if (cat.id === id) {
+      cat.likedByMe = !cat.likedByMe;
+    }
+  }
+  )
+  localStorage.setItem('cats', JSON.stringify(cats));
+  //after click , replace cats with new cats
+  catContainer.innerHTML = '';
+  setCats();
+}
+
+window.addEventListener('load', setCats);

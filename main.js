bb5d73a50da77dac27b6de828634737cf39f4f57
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-06-13 13:33:32 +0300

    finished localStorage

--- main.js
+++ main.js
@@ -1,97 +1,135 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const asd = document.querySelector('.cats__collection'),
-    hearth__two = document.querySelectorAll('.hearth__two'),
-    hearth__three = document.querySelectorAll('.hearth__three'),
-    link = document.querySelectorAll('.link');
+  const hearth = document.querySelectorAll('.hearth__three'),
+    link = document.querySelectorAll('.link'),
+    asd = document.querySelectorAll('.cats');
 
-  let arr = [],
-    itemsArray = [],
-    a,
-    b;
+  const catContainer = document.querySelector('.cats__collection');
 
-  /*Clicked Hearth
-   ********************/
-  const click__hearth = () => {
-    hearth__two.forEach((item, i) => {
-      item.addEventListener('click', () => {
-        if (hearth__three[i].classList.contains != 'hide') {
-          hearth__three[i].classList.remove('hide');
-          hearth__three[i].classList.add('show');
+  const cats = JSON.parse(localStorage.getItem('cats')) || [
+    {
+      img: './assets/img/cat1.png',
+      id: 1,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat2.png',
+      id: 2,
+      likedByMe: true,
+    },
+    {
+      img: './assets/img/cat3.png',
+      id: 3,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat4.png',
+      id: 4,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat5.png',
+      id: 5,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat6.png',
+      id: 6,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat7.png',
+      id: 7,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat8.png',
+      id: 8,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat9.png',
+      id: 9,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat10.png',
+      id: 10,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat11.png',
+      id: 11,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat12.png',
+      id: 12,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat13.png',
+      id: 13,
+      likedByMe: false,
+    },
+    {
+      img: './assets/img/cat14.png',
+      id: 14,
+      likedByMe: false,
+    },
+  ];
 
-          arr.push(hearth__three[i].parentNode);
+  const setCats = () => {
+    cats.forEach((cat) => {
+      const catItem = document.createElement('picture');
+      catItem.classList.add('cats');
+      catItem.addEventListener('click', () => handleLiked(cat.id));
+      catItem.innerHTML = `
+      <img src="${cat.img}" alt="cat">
+      <img class="hearth" src="./assets/icon/Vector.png" alt="hearth" />
+      <div class="hearth__two"></div>
+      ${
+        cat.likedByMe
+          ? '<img class="hearth__three" src="./assets/icon/Vector (2).png" alt="hearth" />'
+          : ''
+      }
+      `;
 
-          itemsArray.push(hearth__three[i].parentNode);
+      catContainer.appendChild(catItem);
 
-          localStorage.setItem('cats', itemsArray);
-          console.log(localStorage.getItem('cats'));
-
-          localStorage.setItem('aaa', JSON.stringify(asd));
-
-          console.log(localStorage.getItem('aaa'));
-
-          /* itemsArray.push(hearth__three[i].parentNode);
-
-          localStorage.setItem('items', JSON.stringify(itemsArray));
-          console.log(localStorage.getItem('items'));
-          console.log(itemsArray); */
-        }
-      });
+      favoriteCats(cat.likedByMe, cat, catItem);
     });
   };
 
-  /* Remove Click Effect
-   ***********************/
-  const unclick__hearth = () => {
-    hearth__three.forEach((item, i) => {
-      item.addEventListener('click', () => {
-        if (hearth__three[i].classList.contains != 'show') {
-          hearth__three[i].classList.remove('show');
-          hearth__three[i].classList.add('hide');
-        }
-      });
+  const handleLiked = (id) => {
+    cats.forEach((cat) => {
+      if (cat.id === id) {
+        cat.likedByMe = !cat.likedByMe;
+      }
     });
+    localStorage.setItem('cats', JSON.stringify(cats));
+    //after click , replace cats with new cats
+    catContainer.innerHTML = '';
+    setCats();
   };
 
-  /* Add Cats to Favorite
-   *************************/
-  const add__favorite = () => {
+  const favoriteCats = (item, cat, block) => {
+    /* Add Cats to Favorite
+     ************************ */
+    if (!cat.likedByMe) console.log(cat.likedByMe);
     link[1].addEventListener('click', () => {
-      hearth__three.forEach((item) => {
-        if (item.classList.contains != 'flex') {
-          item.parentNode.classList.remove('flex');
-          item.parentNode.classList.add('hide');
-        }
-      });
-
-      arr.forEach((key) => {
-        key.append();
-        key.classList.remove('hide');
-        key.classList.add('flex');
-      });
-
-      itemsArray.forEach((key) => {
-        key.append();
-        key.classList.remove('hide');
-        key.classList.add('flex');
-      });
+      if (!cat.likedByMe) {
+        block.classList.remove('flex');
+        block.classList.add('hide');
+      }
     });
-  };
 
-  /* Return All Cats Back
-   ******************************/
-  const return__cats = () => {
     link[0].addEventListener('click', () => {
-      hearth__three.forEach((item) => {
-        if (item.classList.contains != 'hide') {
-          item.parentNode.classList.remove('hide');
-          item.parentNode.classList.add('flex');
-        }
-      });
+      if (!cat.likedByMe) {
+        block.classList.remove('hide');
+        block.classList.add('flex');
+      }
     });
   };
 
-  click__hearth();
-  unclick__hearth();
-  return__cats();
-  add__favorite();
+  window.addEventListener('load', setCats);
 });
